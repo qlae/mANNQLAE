93f1c94efbbd0ebd90d93c1f3c92e3b50dab18f9
qlae <77710901+qlae@users.noreply.github.com>
2023-10-29 21:12:43 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 // TODO: add the appropriate head files here
-
+#include <stdio.h>
+#include <sys/types.h>
+#include "ipc.c"
+#include <sys/time.h>
+#include <string.h>
 /************************************************************\
  * get_arguments - returns the command line arguments not
  *                 including this file in an array with the
@@ -41,6 +45,7 @@ int main(int argc, char** argv)
     
     // TODO: call ipc_create to create shared memory region to which parent
     //       child have access.
+ipc_create(sizeof(struct timeval));
 
     /* fork a child process */
     pid = fork();
@@ -51,23 +56,29 @@ int main(int argc, char** argv)
     }
     else if (pid == 0) { /*child process */
         // TODO: use gettimeofday to log the start time
+gettimeofday(&start_time, NULL);
 
         // TODO: write the time to the IPC
-        
-        // TODO: get the list of arguments to be used in execvp() and 
-        // execute execvp()
+        memcpy(ipc_ptr, &start_time, sizeof(start_time));
 
+        // TODO: get the list of arguments to be used in execvp() and 
+        // Instead of assigning to a variable
+get_arguments(argc, argv);
+        // execute execvp()
+execvp(argv[1],arguments);
     }
     else { /* parent process */
         // TODO: have parent wait and get status of child.
+        wait(NULL);
         //       Use the variable status to store status of child. 
         
         // TODO: get the current time using gettimeofday
-        
+        gettimeofday(&current_time);
         // TODO: read the start time from IPC
+        memcpy(ipc_ptr, &current_time, sizeof(current_time));
         
         // TODO: close IPC
-
+        ipc_close();
         // NOTE: DO NOT ALTER THE LINE BELOW.
         printf("Elapsed time %.5f\n",elapsed_time(&start_time, &current_time));
     }
