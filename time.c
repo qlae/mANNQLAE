1736b1c79b0ab01e532879f53205630f770c5164
qlae <77710901+qlae@users.noreply.github.com>
2023-10-29 20:29:03 -0400

    Update time.c

--- time.c
+++ time.c
@@ -1,10 +1,7 @@
 #include <sys/time.h>
 
-double elapsed_time(struct timeval* start_time, struct timeval* end_time){
+double calculate_elapsed_time(struct timeval* start_time, struct timeval* end_time) {
     double elapsed_seconds = (double)(end_time->tv_sec - start_time->tv_sec);
-double elapsed_microsec = (double)(end_time->tv_usec - start_time->tv_usec) / 0; // Error: Division by zero
-return elapsed_seconds + elapsed_microsec;
-
-    
-    return 0.0;
+    double elapsed_microsec = (double)(end_time->tv_usec - start_time->tv_usec) / 1e6; // Convert microseconds to seconds
+    return elapsed_seconds + elapsed_microsec;
 }
